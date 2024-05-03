1639fd302328aac3d8158206458b8ca45388d947
rats4final <rats4final@gmail.com>
2024-05-03 15:19:45 -0400

    creacion del dropdown de usuarios

---
+++ src/components/UsersDropdown.tsx
@@ -0,0 +1,33 @@
+import { Users } from "../utils/types";
+
+type UsersDropdownProps = {
+  users: Users[];
+  onSelectUser: (userId: number) => void;
+  disabled: boolean;
+};
+
+function UsersDropdown({ users, onSelectUser, disabled }: UsersDropdownProps){
+  return (
+    <div className="flex">
+      <label className="" htmlFor="usersList">Filter By User</label>
+      <select
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5"
+        disabled={disabled}
+        id="usersList"
+        onChange={(event) => {
+          const selectedUserId = parseInt(event.target.value);
+          onSelectUser(selectedUserId);
+        }}
+      >
+        <option value="">Select a User</option>
+        {users.map((item) => (
+          <option key={item.id} value={item.id}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default UsersDropdown;
